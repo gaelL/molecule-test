32f2bad014b5f159fc52d55fb09df223231a95d4
gaelL <contact@gaell.info>
2018-01-23 11:32:01 +0100

    add test infra of uri

--- playbook-test/molecule/default/tests/test_default.py
+++ playbook-test/molecule/default/tests/test_default.py
@@ -12,3 +12,20 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+# Example of URI curl
+
+# With curl
+# def test_curl_homepage(host):
+#     f = host.run("curl -s  localhost:9090/graph | grep '<title>Prometheus'")
+#
+#     assert f.rc == 0
+
+# With ansible
+#def test_homepage(host):
+#
+#    r = host.ansible("uri", "url=http://localhost:9090/graph return_content=yes", check=False)
+#
+#    assert '<title>Prometheus' in r['content']
+
